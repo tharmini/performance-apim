99252f6aa7930c9e2a5ee697515723d538d5096f
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2019-05-24 14:39:41 +0530

    cpus changed

--- distribution/scripts/apim/micro-gw/micro-gw-start.sh
+++ distribution/scripts/apim/micro-gw/micro-gw-start.sh
@@ -23,7 +23,7 @@ label="$default_label"
 default_heap_size="512m"
 heap_size="$default_heap_size"
 micro_gw_version="3.0.0"
-default_cpus="1"
+default_cpus="2"
 cpus="$default_cpus"
 
 function usage() {
